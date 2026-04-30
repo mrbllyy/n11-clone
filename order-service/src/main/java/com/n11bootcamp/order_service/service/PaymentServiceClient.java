c5413d6dbdad60a0c3e424a750d7a8453237cd2f
deb <bilalyay@live.com>
2026-05-01 02:15:24 +0300

    feat: add Feign clients for payment and stock services with Resilience4j integration and base configuration

--- order-service/src/main/java/com/n11bootcamp/order_service/service/PaymentServiceClient.java
+++ order-service/src/main/java/com/n11bootcamp/order_service/service/PaymentServiceClient.java
@@ -5,10 +5,15 @@ import com.n11bootcamp.order_service.dto.payment.PaymentResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+
 @FeignClient(name = "payment-service") // Eureka üzerinden discovery
 public interface PaymentServiceClient {
 
     @PostMapping("/api/payments/pay")
+    @CircuitBreaker(name = "paymentService")
+    @Retry(name = "paymentService")
     PaymentResponse makePayment(PaymentRequest request);
 
 }
--- order-service/src/main/java/com/n11bootcamp/order_service/service/StockServiceClient.java
+++ order-service/src/main/java/com/n11bootcamp/order_service/service/StockServiceClient.java
@@ -6,15 +6,21 @@ import com.n11bootcamp.order_service.dto.stock.StockUpdateResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @FeignClient(name = "stock-service", path = "/api/stocks")
 public interface StockServiceClient {
 
     // 🟢 Stok düşürme (sipariş sırasında)
     @PostMapping("/decrease")
+    @CircuitBreaker(name = "stockService")
+    @Retry(name = "stockService")
     StockUpdateResponse decreaseStock(@RequestBody StockUpdateRequest request);
 
     // 🔵 Stok iadesi (ödeme başarısız vs.)
     @PostMapping("/increase")
+    @CircuitBreaker(name = "stockService")
+    @Retry(name = "stockService")
     StockUpdateResponse increaseStock(@RequestBody StockUpdateRequest request);
 }
